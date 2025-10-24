2bce07c17570a401a052d31bb011032ce934595d
Aishwaryab-5324 <aishwaryab4520@gmail.com>
2025-10-24 14:44:35 +0530

    new

---
+++ recursive.py
@@ -0,0 +1,9 @@
+def rec_fact(n):
+    if n==1:
+        return 1
+    else:
+        return(n*rec_fact(n-1))
+n=int(input("enter a number:"))
+if n>=1:
+    print("factorial of a number is",rec_fact(n))
+    
---
+++ sum_lambda.py
@@ -0,0 +1,4 @@
+sum=lambda x,y:x+y
+x=int(input("enter the value of x:"))
+y=int(input("enter the value of y:"))
+print("sum",sum(x,y))
